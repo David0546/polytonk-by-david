99369bc6cb612e44c3b4d5d41fd0a0dfeff493d1
GitLocked <93531478+GitLocked@users.noreply.github.com>
2022-11-25 10:56:23 -0600

    Create config.js

---
+++ client/config.js
@@ -0,0 +1,11 @@
+window.beta = false;
+window.servers = [{
+  wsLink: (window.top.location.protocol == "https:" ? "wss://" : "ws://") + window.top.location.host + "/server",
+  name: ""
+}]
+// Year, Month, Full Date
+window.changelogDate = [1642606410699]; // SET CHANGELOG TO YOUR CURRENT DATE AFTER MAKING A CHANGE! TO GET YOUR DATE, PUT Date.now() ON YOUR CONSOLE
+window.changelog = ["Add your own changelog!"];
+//window.changelog = ["Added 4TDM", "Machine Gun reload is now slower", "Penetration now does stuff", "Added autofire", "You can now upgrade your stats/skills with numbers in your key", "Fixed a bug, where all of the bullet healths were the same, causing bullets to be extremely weak", "Fixed Triple Shot, Destroyer and Annihilator design (Suggested by Oblivion)", "Nerfed Assassin, Sniper and Ranger spray", "Buffed movement speed skill", "Added Negev", "Fixed a circle rendering issue", "Adjusted the UI a lot", "Adjusted the UI size to be better for every screen", "Fixed a bug where the tanks didnt grow on death animation", "Pressing enter on death screen now redirects you to the menu", "Changed connecting screen background to be a grid", "Finally implemented reload delay, no more cursed tanks", "Added giant polygons like alpha pentagon, but more valueable and bigger"];
+window.inComingChangelog = ["Added 4TDM", "Machine Gun reload is now slower", "Penetration now does stuff", "Added autofire", "You can now upgrade your stats/skills with numbers in your key", "Fixed a bug, where all of the bullet healths were the same, causing bullets to be extremely weak", "Fixed Destroyer and Annihilator design (Suggested by Oblivion)", "Nerfed Assassin, Sniper and Ranger spray", "Buffed movement speed skill", "Added Negev", "Fixed a circle rendering issue", "Adjusted the UI a lot", "Adjusted the UI size to be better for every screen", "Fixed a bug where the tanks didnt grow on death animation", "Pressing enter on death screen now redirects you to the menu", "Changed connecting screen background to be a grid", "Finally implemented reload delay, no more cursed tanks"];
+window.previousChangelogs = [["Rightclicking now doesnt open the menu"]];
